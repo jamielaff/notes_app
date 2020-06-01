054c22f34d2f8c2ccac072f305db1908a85b8f4c
jamielaff <jamielafferty99@gmail.com>
2020-06-01 22:14:15 +0100

    Sessions feature spec, covering bad login, good login and good logout

---
+++ spec/features/sessions_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.feature 'Sessions management', tpye: :feature do
+  scenario 'User tries to login with no data' do 
+    visit login_path
+    click_button 'Log in'
+
+    expect(page).to have_text('There was something wrong with your login information')
+  end
+
+  scenario 'User tries to login with valid data' do 
+    user = User.create(username: 'test_user', password: 'password', email: 'email@gmail.com')
+
+    visit login_path
+    fill_in 'username', with: user.username
+    fill_in 'password', with: user.password
+    click_button 'Log in'
+
+    expect(current_path).to eq(root_path)
+    expect(page).to have_text('You have logged in')
+  end
+
+  scenario 'User tries to logout when logged in' do 
+    user = User.create(username: 'test_user', password: 'password', email: 'email@gmail.com')
+
+    visit login_path
+    fill_in 'username', with: user.username
+    fill_in 'password', with: user.password
+    click_button 'Log in'
+
+    expect(current_path).to eq(root_path)
+    expect(page).to have_text('You have logged in')
+
+    click_link 'Log out'
+    expect(current_path).to eq(root_path)
+    expect(page).to have_text('You have logged out')
+  end
+end
