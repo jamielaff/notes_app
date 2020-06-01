292b0443c821f94e8e771d825a98390859191812
jamielaff <jamielafferty99@gmail.com>
2020-06-01 20:15:09 +0100

    Login/logout & flash messages. Add login & logout button to navbar (depending on logged_in). Add flash messages for login logout & pass email/password.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,23 @@
 class SessionsController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      flash[:success] = "You have logged in"
+      redirect_to root_path
+    else
+      flash.now[:danger] = "There was something wrong with your login information"
+      render 'new'
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have logged out"
+    redirect_to root_path
   end
 end
