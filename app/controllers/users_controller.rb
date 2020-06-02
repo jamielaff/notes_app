b00567ca5204c057723e48b7542d6f0993a184b2
jamielaff <jamielafferty99@gmail.com>
2020-06-02 22:05:23 +0100

    Users controller functions User can be CREATED only by admin. User be READ by anyone. User can be UPDATED only by self or admin (WIP) User can be DELETED only by self or admin (WIP) Private functions created to perform checks, along with before actions to ensure

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,60 @@
 class UsersController < ApplicationController
+  skip_before_action :authorised, only: [:index, :new, :create, :show]
+
+  before_action :authorised_for_user_actions, except: [:index, :show]
+
   def index
+    @users = User.all
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Created new user, #{@user.username}"
+      redirect_to user_path(@user)
+    else
+      render 'new'
+     end
   end
 
   def edit
+    @user = current_user
   end
 
   def update
+    @user = current_user
+    if @user.update(user_params)
+      flash[:success] = "#{@user.username}'s account was updated successfully"
+      redirect_to user_path(@user)
+    else
+      render 'edit'
+    end
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def destroy
+    @user.destroy
+    flash[:success] = "User was deleted"
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :is_admin)
+  end
+
+  def authorised_for_user_actions
+    unless logged_in_as_admin?
+      flash[:danger] = "You are not authorised to perform that action"
+      redirect_to root_path
+    end
   end
 end
