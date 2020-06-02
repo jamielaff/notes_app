021636beaeec50b8fb43b2a16a6a0c1ee3c1a512
jamielaff <jamielafferty99@gmail.com>
2020-06-02 13:42:46 +0100

    Allow unknown URLs in Capybara

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,6 +33,11 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
+Capybara::Webkit.configure do |config|
+  # Allow unknown URLs in Capybara
+  config.allow_unknown_urls
+end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
