0ae9b782950ad2c9246920fc7fbeeef093daf1c6
jamielaff <jamielafferty99@gmail.com>
2020-06-02 13:58:41 +0100

    Remove capybara config from spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,10 +13,6 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
-Capybara::Webkit.configure do |config|
-  # Allow unknown URLs in Capybara
-  config.allow_urls("www.example.com") 
-end
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
