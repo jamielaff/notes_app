eaae8e0937f949c24cd6373664344f00a335d71a
jamielaff <jamielafferty99@gmail.com>
2020-06-03 19:19:09 +0100

    Delete Admin can delete themselves & any user team member can only delete themselves

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,6 +40,7 @@ class UsersController < ApplicationController
   end
 
   def destroy
+    @user = User.find(params[:id])
     @user.destroy
     flash[:success] = "User was deleted"
     redirect_to root_path
