0b521e2fbecec25309cd93ee9eef5828e1292d18
jamielaff <jamielafferty99@gmail.com>
2020-06-06 22:21:06 +0100

    Specs for new moderation features

--- spec/factories/note.rb
+++ spec/factories/note.rb
@@ -3,11 +3,19 @@ FactoryBot.define do
     user
     title       { 'Title of team member note' }
     description { 'Description of team member note' }
+    is_active   { true }
   end
 
   factory :admin_note, class: 'note' do
     user
-    title       { 'Title of team member note' }
-    description { 'Description of team member note' }
+    title       { 'Title of admin note' }
+    description { 'Description of admin note' }
+    is_active   { true }
+  end
+
+  factory :pending_note, class: 'note' do
+    user
+    title       { 'Title of pending note' }
+    description { 'Description of pending note' }
   end
 end
