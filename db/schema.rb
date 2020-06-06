f9304188c9b668fe3b17bd10f11f0a9306019891
jamielaff <jamielafferty99@gmail.com>
2020-06-06 22:20:48 +0100

    DB migration for new is_active flag, defaulted to false

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_04_193013) do
+ActiveRecord::Schema.define(version: 2020_06_06_184451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_06_04_193013) do
     t.text "description", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "is_active", default: false
     t.index ["user_id"], name: "index_notes_on_user_id"
   end
 
