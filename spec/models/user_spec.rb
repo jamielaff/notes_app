18a3afc4e5b1660bfb0cd1bff16ba35a26c87ff3
jamielaff <jamielafferty99@gmail.com>
2020-06-07 16:25:59 +0100

    Use the created user object for test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ RSpec.feature User, type: :model do
   describe 'New user' do
     it 'Will default to non admin' do
       user = User.create(username: 'user', password: 'password', email: 'email@email.com')
-      expect(User.first.is_admin?).to eq(false)
+      expect(user.is_admin?).to eq(false)
     end
   end
 end
