fc4a06ca1684470308d8625ca6b3cd00deafd7ad
jamielaff <jamielafferty99@gmail.com>
2020-06-07 14:20:19 +0100

    Comment on active method

--- app/models/note.rb
+++ app/models/note.rb
@@ -11,6 +11,7 @@ class Note < ActiveRecord::Base
     user == user_to_verify
   end
 
+  # In retrospect, the column should be called active not is_active so this method would not be needed
   def active?
     is_active
   end
