dc99b41afd1049f26e26e32fbfe3d951ce372f0e
jamielaff <jamielafferty99@gmail.com>
2020-06-07 16:24:31 +0100

    Remove user form note factory (not necessary)

--- spec/factories/note.rb
+++ spec/factories/note.rb
@@ -1,20 +1,17 @@
 FactoryBot.define do
   factory :note, class: 'note' do
-    user
     title       { 'Title of team member note' }
     description { 'Description of team member note' }
     is_active   { true }
   end
 
   factory :admin_note, class: 'note' do
-    user
     title       { 'Title of admin note' }
     description { 'Description of admin note' }
     is_active   { true }
   end
 
   factory :pending_note, class: 'note' do
-    user
     title       { 'Title of pending note' }
     description { 'Description of pending note' }
   end
