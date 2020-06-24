2825c214f0fbef29a673b643f44e4320cab548f5
jamielaff <jamielafferty99@gmail.com>
2020-06-24 17:10:55 +0100

    Small changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,8 @@ class ApplicationController < ActionController::Base
   helper_method :logged_in_as_admin?
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    return unless session[:user_id]
+    @current_user ||= User.find(session[:user_id])
   end
 
   def logged_in?
