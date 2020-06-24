45b1f9d566eb4f287651c526394e100805089bc3
jamielaff <jamielafferty99@gmail.com>
2020-06-24 17:57:51 +0100

    More JWT stuff

--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,22 @@ Rails.application.routes.draw do
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
   post '/graphql',  to: 'graphql#execute'
-  get 'login',      to: 'sessions#new'
-  post 'login',     to: 'sessions#create'
-  delete 'logout',  to: 'sessions#destroy'
-  get 'signup',     to: 'users#new'
+  
+  namespace api do
+    namespace v1 do:
+      get 'login',      to: 'sessions#new'
+      post 'login',     to: 'sessions#create'
+      delete 'logout',  to: 'sessions#destroy'
+      get 'signup',     to: 'users#new'
 
-  resources :users
-  resources :notes, except: [:index] do
-    collection do
-      get :pending
+      resources :users
+      resources :notes, except: [:index] do
+        collection do
+          get :pending
+        end
+        put :approve
+      end
     end
-    put :approve
   end
 
   root 'notes#index'
