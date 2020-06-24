1bb698b7a8904edce7bf190e6a7dcbfec3ccef75
jamielaff <jamielafferty99@gmail.com>
2020-06-24 17:12:31 +0100

    User type

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -1,5 +1,7 @@
 module Types
   class UserType < Types::BaseObject
+    graphql_name 'User'
+    
     field :id, ID, null: false
     field :username, String, null: false
     field :email, String, null: false
